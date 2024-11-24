fad895f6de21b89d6911f76410758e4834c0497b
Mehak Fatima <mehakfatimajuw@gmail.com>
2024-11-24 13:41:47 +0500

    Added some products and displayed it

---
+++ src/main/java/com/project/BlogGenie/controllers/ProductsController.java
@@ -0,0 +1,31 @@
+package com.project.BlogGenie.controllers;
+
+import com.project.BlogGenie.models.Product;
+import com.project.BlogGenie.services.ProductsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/products")
+public class ProductsController {
+//  isy service container se request krna hai islye autowired annotation use ki
+    @Autowired
+    private ProductsRepository repo;
+
+//    method that reads products from database
+
+    @GetMapping({"", "/"}) // ye url /products se accessible hai
+//    ye method http get method se accessible hga islye ye annotation
+    public String showProductList(Model model){
+//        List<Product> products = repo.findAll(); // in ascending
+        List<Product> products = repo.findAll(Sort.by(Sort.Direction.DESC, "id")); // in ascending
+        model.addAttribute("products", products);
+        return "products/index"; // name of html file which will return
+    }
+}
